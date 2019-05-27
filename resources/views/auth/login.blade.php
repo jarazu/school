8349c952892aa2dcce0226fd85c68c22dae93b1f
Razu <jahangir8988@gmail.com>
2019-05-28 01:13:58 +0600

    initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,146 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+@section('content')
+<!-- <div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2" id="main-container">
+            <div class="panel panel-default">
+                <div class="page-panel-title">Login</div>
+                
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Or Phone Number</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        {{--
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        --}}
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+                                {{--
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                                --}}
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2" id="main-container">
+            <div class="panel panel-default">
+                <div class="wrapper fadeInDown">
+                    <div id="formContent">
+                        <!-- Tabs Titles -->
+                        @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
+
+                        <!-- Icon -->
+                        <div class="fadeIn first mt-20 mb-20">
+                            <img src="{{asset('image/school.png')}}" id="icon" alt="User Icon" />
+                        </div>
+
+                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+                            <!-- Login Form -->
+                            <form>
+                                <!-- <input type="text" id="login" class="fadeIn second" name="login" placeholder="login"> -->
+                                <input id="email" type="text" class="fadeIn second" name="email"
+                                    value="{{ old('email') }}" required autofocus placeholder="login">
+
+                                @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                                @endif
+                                <!-- ########## -->
+                                <input type="password" id="password" class="fadeIn third" name="password"
+                                    placeholder="password">
+
+                                <!-- <input id="password" type="password" class="fadeIn third" name="password" required  placeholder="password"> -->
+
+                                @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                                @endif
+                                <!-- ################# -->
+                                <input type="submit" class="fadeIn fourth mt-20" value="Log In">
+                            </form>
+
+                            <!-- Remind Passowrd -->
+                            <div id="formFooter">
+                                <a class="underlineHover" href="#">Forgot Password?</a>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
